81519be3f3ffe8abf27445dba1d270eaf241381a
4onen <mattjdupree@gmail.com>
2024-08-15 23:59:44 -0500

    In sample WebGL code drop surprisingly expensive isNaN

--- game/g.js
+++ game/g.js
@@ -78,10 +78,10 @@ gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
 gl.uniform1i(tex_loc, 0);
 gl.clearColor(0, 0, 0, 1);
 
-let first = NaN;
+let first = null;
 function drawscene(t) {
   resize_canvas_to_display(gl);
-  if (isNaN(first)) first = t;
+  if (first === null) first = t;
   gl.clear(gl.COLOR_BUFFER_BIT);
   const num_points = 5;
   for (let i = 0; i < num_points; i++) {
