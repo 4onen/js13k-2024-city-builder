9c18e239912ba6c9e1ddf9e8b3f65e3da827d14a
4onen <mattjdupree@gmail.com>
2024-08-16 00:35:45 -0500

    Add smoothed FPS calculation and counter.

--- game/g.js
+++ game/g.js
@@ -57,6 +57,7 @@ const resize_canvas_to_display = (gl) => {
   gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
 };
 const canvas = document.querySelector('canvas');
+const fpsel = document.getElementById('fps');
 const gl = canvas.getContext('webgl2');
 gl.pixelStorei(gl.UNPACK_ALIGNMENT, 1);
 const prog = glProgFromSrc(gl, vs, fs);
@@ -78,23 +79,53 @@ gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
 gl.uniform1i(tex_loc, 0);
 gl.clearColor(0, 0, 0, 1);
 
-let first = null;
+let first_time = null;
+let last_time = null;
+let frame_num = 0;
+const TARGET_FPS = 60;
+const TARGET_DT = 1 / TARGET_FPS;
+const FRAMES_FPS_SMOOTHING = 10;
+const FPS_UPDATE_INTERVAL = 0.25;
+function calc_dt(t) {
+  frame_num += 1;
+  if (first_time === null) first_time = t;
+  if (last_time === null) last_time = t;
+  const dt = t - last_time > 0 ? t - last_time : TARGET_DT;
+  return dt * 0.001;
+}
+let last_fps = null;
+function calc_fps(dt) {
+  if (last_fps === null) { return last_fps = TARGET_FPS; }
+  const frames_fps_smoothing = Math.min(FRAMES_FPS_SMOOTHING, frame_num);
+  const fps = (frames_fps_smoothing * last_fps) / (dt * last_fps + frames_fps_smoothing - 1);
+  last_fps = fps;
+  return fps;
+}
+let time_to_fps_update = FPS_UPDATE_INTERVAL;
+function update_fps(fps, dt) {
+  time_to_fps_update -= dt;
+  while (time_to_fps_update < 0) {
+    time_to_fps_update += FPS_UPDATE_INTERVAL;
+    fpsel.textContent = fps.toFixed(1);
+  }
+}
 function drawscene(t) {
+  const dt = calc_dt(t);
+  const fps = calc_fps(dt);
+  update_fps(fps, dt);
   resize_canvas_to_display(gl);
-  if (first === null) first = t;
   gl.clear(gl.COLOR_BUFFER_BIT);
   const num_points = 5;
   for (let i = 0; i < num_points; i++) {
     const u = i / (num_points - 1); // 0 to 1
     const clipspace = u * 1.6 - 0.8; // -0.8 to 0.8
-    gl.vertexAttrib4f(pos_loc, clipspace, (Math.sin((t - first) / 1000 + u)) * 0.8, 0, 1);
+    gl.vertexAttrib4f(pos_loc, clipspace, (Math.sin((t - first_time) / 1000 + u)) * 0.8, 0, 1);
     const offset = 0;
     const count = 1;
     gl.drawArrays(gl.POINTS, offset, count);
   }
+  last_time = t;
   requestAnimationFrame(drawscene);
 }
-
-requestAnimationFrame(drawscene);
-
 resize_canvas_to_display(gl);
+requestAnimationFrame(drawscene);
