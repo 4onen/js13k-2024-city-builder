80db437ecf263fc32d1d9e3f1b74a1bd72ba5352
4onen <mattjdupree@gmail.com>
2024-09-13 01:50:55 -0500

    Add colons to the whitespace removal in JS and CSS for decent savings.

--- script/minifier.py
+++ script/minifier.py
@@ -11,12 +11,14 @@ WHITESPACE = re.compile(r"\s+")
 JS_COMMENT = re.compile(r"/\*.*?\*/", re.DOTALL)
 JS_ONELINE_COMMENT = re.compile(r"//.*?\n")
 JS_AFTER_SEMICOLON = re.compile(r";\s*")
+JS_AFTER_COLON = re.compile(r":\s*")
 JS_AFTER_COMMA = re.compile(r",\s*")
 JS_AFTER_OPEN_CURLY = re.compile(r"\{\s*")
 JS_AFTER_CLOSE_CURLY = re.compile(r"\}\s*")
 JS_AFTER_OPEN_BRACKET = re.compile(r"\[\s*")
 JS_AFTER_CLOSE_BRACKET = re.compile(r"\]\s*")
 JS_CONST = re.compile(r"\bconst\b")  # Breaks shaders
+JS_SPACE_AROUND_EQUAL = re.compile(r"\s*=\s*")
 HTML_COMMENT = re.compile(r"<!--.*?-->", re.DOTALL)
 HTML_WHITESPACE = re.compile(r">\s+<")
 
@@ -25,11 +27,13 @@ def minify_js(js: str) -> str:
     js = JS_COMMENT.sub("", js)
     js = JS_ONELINE_COMMENT.sub("", js)
     js = JS_AFTER_SEMICOLON.sub(";", js)
+    js = JS_AFTER_COLON.sub(":", js)
     js = JS_AFTER_COMMA.sub(",", js)
     js = JS_AFTER_OPEN_CURLY.sub("{", js)
     js = JS_AFTER_CLOSE_CURLY.sub("}", js)
     js = JS_AFTER_OPEN_BRACKET.sub("[", js)
     js = JS_AFTER_CLOSE_BRACKET.sub("]", js)
+    js = JS_SPACE_AROUND_EQUAL.sub("=", js)
     # js = JS_CONST.sub('let', js) # Breaks shaders
     # js = WHITESPACE.sub(' ', js) # Breaks shaders
     return js
@@ -39,6 +43,7 @@ def minify_html(html: str) -> str:
     html = HTML_COMMENT.sub("", html)
     html = HTML_WHITESPACE.sub("><", html)
     html = JS_AFTER_SEMICOLON.sub(";", html)  # For CSS
+    html = JS_AFTER_COLON.sub(":", html)  # For CSS
     html = JS_AFTER_OPEN_CURLY.sub("{", html)  # For CSS
     html = JS_AFTER_CLOSE_CURLY.sub("}", html)  # For CSS
     html = JS_COMMENT.sub("", html)  # For CSS
